8cb2da92d6052d85e903d190b903ce355c0f15ce
Hemant Topiwala <hemanttopiwala212@gmail.com>
2021-12-17 16:52:01 +0530

    basic file structure setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const path=require('path');
+const fs=require('fs');
+const app=express();
+const port=80;
+
+
+
+app.use('/static' ,express.static('static')); //for serving the static files
+app.use(express.urlencoded());
+
+app.set('view-engine','pug');// set the view templete engine
+app.set('views',path.join(__dirname, 'views'));
+
+//endpoints
+app.get('/',(req,res)=>{
+    const params={}
+    res.status(200).render('index.pug',params);
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`server run successfully on port ${port}`);
+});
